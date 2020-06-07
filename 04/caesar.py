7d867cc04e867d1cbe25afdf9f362f06217896bc
Alexandra <sasa.matulova@gmail.com>
2020-06-07 22:13:09 +0200

    Homework caesar

--- 04/caesar.py
+++ 04/caesar.py
@@ -2,26 +2,27 @@ key = int(input('What key do you want to use? '))
 plaintext = input('What text do you want to encrypt? ')
 ciphertext = ''
 
+if key <= 0:
+    print('Please use positive numbers only')
+
 plaintext_list = list(plaintext)
 for i in range(len(plaintext)):
-    #print(plaintext_list[i])
-    #print(ord(plaintext_list[i]))
-    #print(key)
     pi = ord(plaintext_list[i])
+    #small letters
     if pi >= 97 and pi <= 122:
-        print(pi)
-        print((pi + key) % pi)
-        if pi + key > 122:
+        key = key % 26
+        if pi + key <= 122:
+            ci = pi + key
+        else:
             ci = 97 + (pi + key) % 123
-        else:
-            ci = pi + key
+    #capital letters
     elif pi >= 65 and pi <= 90:
-        if pi + key > 90:
-            ci = 65 + (pi + key) % 91
+        if pi + key <= 90:
+            ci = pi + key   
         else:
-            ci = pi + key
+            ci = 65 + (pi + key) % 91    
     else:
         ci = pi
     ciphertext = ciphertext + chr(ci)
 
-print(ciphertext)
+print('result', ciphertext)
