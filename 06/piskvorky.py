ee7f6cb07f7efe5969ccdc92e2164576140d8b1d
Alexandra <sasa.matulova@gmail.com>
2020-06-27 20:16:55 +0200

    testy

--- 06/piskvorky.py
+++ 06/piskvorky.py
@@ -1,6 +1,7 @@
 from random import randrange
+import ai
 
-# ukol 9
+
 def vyhodnot(herni_pole):
     if 'xxx' in herni_pole:
         return 'x'
@@ -13,7 +14,7 @@ def vyhodnot(herni_pole):
 
 #print(vyhodnot('----------ooo-------'))
 
-# ukol 10
+
 herni_pole = '--------------------'
 cislo_policka = 0
 symbol = ''
@@ -26,7 +27,7 @@ def tah(herni_pole, cislo_policka, symbol):
 
 #print(tah(herni_pole, 2, 'x'))
 
-# ukol 11
+
 herni_pole = '--------------------'
 def tah_hrace(herni_pole):
     cislo_policka = int(input('Na kterou pozici chces hrat? (Zadej cislo od 0 do 19) ')) 
@@ -38,19 +39,7 @@ def tah_hrace(herni_pole):
 
 #print(tah_hrace(herni_pole))
 
-# ukol 12
-def tah_pocitace(herni_pole):
-    cislo_policka = randrange(0, 19)
-    herni_pole_list = list(herni_pole)
-    while herni_pole_list[cislo_policka] == 'x' or herni_pole_list[cislo_policka] == 'o':
-        cislo_policka = randrange(0, 19)
-    herni_pole_list[cislo_policka] = 'o'
-    herni_pole = ''.join(herni_pole_list)
-    return herni_pole
 
-#print(tah_pocitace(herni_pole))
-
-# ukol 13
 def piskvorky1d():
     herni_pole = '--------------------'
     while vyhodnot(herni_pole) != 'x' or vyhodnot(herni_pole) != 'o' or vyhodnot(herni_pole) != '!':
@@ -59,13 +48,12 @@ def piskvorky1d():
         if vyhodnot(herni_pole) == 'x' or vyhodnot(herni_pole) == 'o' or vyhodnot(herni_pole) == '!':
             return 'Vyhral jsi'
         else:
-            herni_pole = tah_pocitace(herni_pole)
+            herni_pole = ai.tah_pocitace(herni_pole)
             print('pole po tahu pocitace:', herni_pole)
             if vyhodnot(herni_pole) == 'x' or vyhodnot(herni_pole) == 'o' or vyhodnot(herni_pole) == '!':
                 return 'Pocitac vyhral'
 
-print(piskvorky1d())
-
-    
+#if _main_ == '_name_':
+#   piskvorky1d() 
 
     
