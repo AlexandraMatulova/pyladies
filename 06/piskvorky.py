97a856c319aac4f5c4ecd26865fe0f24645838dd
Alexandra <sasa.matulova@gmail.com>
2020-06-28 22:20:20 +0200

    Dalsi testy

--- 06/piskvorky.py
+++ 06/piskvorky.py
@@ -21,7 +21,15 @@ symbol = ''
 
 def tah(herni_pole, cislo_policka, symbol):
     herni_pole_list = list(herni_pole)
-    herni_pole_list[cislo_policka] = symbol
+    try:
+        if herni_pole_list[cislo_policka] == '-':
+            herni_pole_list[cislo_policka] = symbol
+        else:
+            print('Toto policko je uz obsazene, zkus to znovu.')
+            return herni_pole
+    except IndexError:
+        print('Nezadal jsi cislo od 0 do 19. Zkus to znovu.')
+        return herni_pole
     herni_pole = ''.join(herni_pole_list)
     return herni_pole
 
@@ -31,10 +39,7 @@ def tah(herni_pole, cislo_policka, symbol):
 herni_pole = '--------------------'
 def tah_hrace(herni_pole):
     cislo_policka = int(input('Na kterou pozici chces hrat? (Zadej cislo od 0 do 19) ')) 
-    herni_pole_list = list(herni_pole)
-    while herni_pole_list[cislo_policka] == 'x' or herni_pole_list[cislo_policka] == 'o':
-        print('Toto policko je uz obsazene')
-        cislo_policka = int(input('Zadej jine cislo (od 0 do 19)'))
+
     return tah(herni_pole, cislo_policka, 'x')
 
 #print(tah_hrace(herni_pole))
@@ -42,18 +47,21 @@ def tah_hrace(herni_pole):
 
 def piskvorky1d():
     herni_pole = '--------------------'
-    while vyhodnot(herni_pole) != 'x' or vyhodnot(herni_pole) != 'o' or vyhodnot(herni_pole) != '!':
-        herni_pole = tah_hrace(herni_pole)
+    while vyhodnot(herni_pole) == '-':
+        tmp_herni_pole = herni_pole  
+        while tmp_herni_pole == herni_pole:
+            herni_pole = tah_hrace(herni_pole)
         print('pole po tahu hrace:', herni_pole)
-        if vyhodnot(herni_pole) == 'x' or vyhodnot(herni_pole) == 'o' or vyhodnot(herni_pole) == '!':
+        if vyhodnot(herni_pole) == 'x':
             return 'Vyhral jsi'
+        elif vyhodnot(herni_pole) == '!':
+            return 'Remiza'
         else:
             herni_pole = ai.tah_pocitace(herni_pole)
             print('pole po tahu pocitace:', herni_pole)
-            if vyhodnot(herni_pole) == 'x' or vyhodnot(herni_pole) == 'o' or vyhodnot(herni_pole) == '!':
+            if vyhodnot(herni_pole) == 'o':
                 return 'Pocitac vyhral'
-
-#if _main_ == '_name_':
-#   piskvorky1d() 
+            elif vyhodnot(herni_pole) == '!':
+                return 'Remiza'
 
     
--- 06/test_piskvorky.py
+++ 06/test_piskvorky.py
@@ -1,4 +1,5 @@
 import piskvorky
+import ai
 
 
 def test_tah_check_index():
@@ -8,5 +9,33 @@ def test_tah_check_index():
     herni_pole = piskvorky.tah(herni_pole, cislo_policka, symbol)
     assert herni_pole == 'x-------------------'
 
-def test_tah
+def test_tah_cislo_policka():
+    herni_pole = '--------------------'
+    cislo_policka = 23
+    symbol = 'x'
+    herni_pole = piskvorky.tah(herni_pole, cislo_policka, symbol)
+    assert herni_pole == '--------------------'
+
+def test_vyhodnot_prazdne_pole():
+    herni_pole = '--------------------'
+    assert piskvorky.vyhodnot(herni_pole) == '-'
+
+def test_vyhodnot_pocitac_vyhral():
+    herni_pole = 'ooo-----------------'
+    assert piskvorky.vyhodnot(herni_pole) == 'o'
+
+def test_tah_pocitace_zmena_pole():
+    herni_pole = '--------------------'
+    assert ai.tah_pocitace(herni_pole) != herni_pole
+
+def test_tah_pocitace_zmenen_pouze_spravny_znak():
+    herni_pole = '--------------------'
+    assert ai.tah_pocitace(herni_pole).count('o') == 1
+    assert ai.tah_pocitace(herni_pole).count('x') == 0
+    assert len(ai.tah_pocitace(herni_pole)) == len(herni_pole)
+
+
+
+
+
 
